2cba5b2422e45c4e8d4ff03744154dcdd298fdaf
Mileto Di Marco <mileto.di.marco@howest.be>
2021-11-29 14:27:15 +0100

    Add dummy login page

---
+++ Wba.EfBasics.Web/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Wba.EfBasics.Web.ViewModels;
+
+namespace Wba.EfBasics.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(AccountLoginViewModel accountLoginViewModel)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(accountLoginViewModel);
+            }
+            //check user  and
+            //check credentials
+            
+            //log in
+            return RedirectToAction("Index", "Courses");
+        }
+
+    }
+}
